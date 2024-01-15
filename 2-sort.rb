452fdfc44dcfd3e2565b2948952834cc98dcf1b6
Mohamed Eladly <msrtv365@yahoo.com>
2024-01-15 16:42:00 +0200

    Fixed task 2-sort.rb

--- 2-sort.rb
+++ 2-sort.rb
@@ -1,17 +1,19 @@
+#!/usr/bin/env ruby
 ###
 #
-#  Sort integer arguments (ascending) 
+#  Sort integer arguments (ascending)
 #
 ###
 
 result = []
 ARGV.each do |arg|
+  # 12 41 2 C 9 -9 31 fun -1 32
     # skip if not integer
     next if arg !~ /^-?[0-9]+$/
 
     # convert to integer
     i_arg = arg.to_i
-    
+
     # insert result at the right position
     is_inserted = false
     i = 0
@@ -20,9 +22,10 @@ ARGV.each do |arg|
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
-            is_inserted = true
-            break
+            tmp = result[i]
+            result[i] = i_arg
+            i_arg = tmp
+            i += 1
         end
     end
     result << i_arg if !is_inserted
