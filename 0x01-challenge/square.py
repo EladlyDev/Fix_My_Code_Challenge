3297e7d3c6ea39f8f06bf83fc3c2ffed18856344
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-18 05:41:07 +0200

    2. Step 2: User model

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+""" Contains the square class """
+
 
 class square():
-    
+    """ the square class which defines a square """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ Called by default for initialization """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,13 +17,15 @@ class square():
         return self.height * self.height
 
     def PermiterOfMySquare(self):
+        """ Permiter of the square """
         return self.height * 4
 
     def __str__(self):
+        """ magic method that returns a peek of the square """
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
